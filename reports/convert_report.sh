0c43b83b355125407dad87b4d9101e5ede54d4cd
WVDevCode <werner.devcode@gmail.com>
2025-04-14 17:46:39 +0200

    Updated jupyter to show full images. Cleaned up life_expectancy_analysis report phase 2. Added jupyter template for future modification.

--- reports/convert_report.sh
+++ reports/convert_report.sh
@@ -7,16 +7,19 @@ echo "current directory: $(pwd)"
 echo "Checking if notebook exists..."
 ls -l "$NOTEBOOK"
 
-
 echo "Converting $NOTEBOOK to PDF with hidden tagged inputs..."
+
 jupyter nbconvert "$NOTEBOOK" \
-    --to pdf \
+    --to webpdf \
     --TagRemovePreprocessor.enabled=True \
-    --TagRemovePreprocessor.remove_input_tags='["hide_input"]'
+    --TagRemovePreprocessor.remove_input_tags='["hide_input"]' \
+    --TagRemovePreprocessor.remove_output_tags='["hide_output"]' \
+    --TagRemovePreprocessor.remove_cell_tags='["hide_cell"]' \
+    --no-prompt     
 
 if [ $? -eq 0 ]; then
     echo "Conversion successful! Opening the PDF..."
-    start "$OUTPUT_PDF"
+#    start "$OUTPUT_PDF"
 else
     echo "Error: Conversion failed."
 
